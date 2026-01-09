1dff09e154032dc51f096ecb082e6f1503254b3f
charles <charlesebuka1@gmail.com>
2026-01-09 14:58:12 +0100

    all

---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       return PostResource::collection(Post::with('author')->paginate(2));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+     {
+        $data = $request->validated();
+        $data['author_id'] = 1;
+        $post = Post::create($data);
+        return new PostResource($post);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+         return new PostResource($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+       $data = $request->validate([
+            'title' => 'required|string|min:2',
+            'body' => ['required','string','min:2']
+        ]);
+        $data['author_id'] = 1;
+        $post->update($data);
+        return new PostResource($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->noContent();
+    }
+}
