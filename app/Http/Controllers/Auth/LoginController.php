b5877ea5dac3a9dd88e67244d8bbffbc17fee5bf
charles <charlesebuka1@gmail.com>
2026-01-16 11:30:35 +0100

    okay

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\USerResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): array
+    {
+        $request->authenticate();
+        $user = $request->user();
+        $token = $request->createToken('main')->plainTextToken;
+
+        return [
+            'user' => new USerResource($user),
+            'token' => $token
+        ];
+    }
+
+    /**
+     * Destwroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::prefix('v1')->group(function(){
-Route::apiResource();
-});
+Route::prefix('v1')->group(function () {
+    Route::apiResource('posts', PostController::class);
+}); 
+
+require __DIR__.'/auth.php';
